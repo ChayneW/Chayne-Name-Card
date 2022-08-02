cf0f84a98e43d300adb826e152af99769896dea6
Chaynes-MacBookPro <ChayneW-MacBookPro.local>
2022-08-02 16:56:11 -0400

    added secret key

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'fewfewf56482fwefds48ght'
+
 Bootstrap(app)
 
 @app.route("/home")
